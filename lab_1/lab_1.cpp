6ee730c491380b42149955afc7b454a1bf70d309
MercyFlesh <con2288@mail.ru>
2020-02-20 00:47:47 +0300

    fix lab_1

--- lab_1/lab_1.cpp
+++ lab_1/lab_1.cpp
@@ -4,31 +4,55 @@
 
 using namespace std;
 
-void* threadOneFunct(void* thread_data)
+
+void* threadOneFunct(void* args)
 {
+    cout << "Started thread\t" << pthread_self() << endl;
+    *((long*) args) = pthread_self();
+    
     for (int i = 0; i < 10; i++)
     {
-        cout << "thread id:\t" << pthread_self() << endl;
-        sleep(5);
+        cout << "thread id:\t" << pthread_self() << "\titer: " << i << endl;
+        sleep(3);
     }
+    
+    pthread_exit(0);
 }
 
-void* threadTwoFunct(void* thread_data)
+
+void* threadTwoFunct(void* args)
 {
+    cout << "Started thread\t" << pthread_self() << endl;
+    *((long*) args) = pthread_self();
+
     for (int i = 0; i < 10; i++)
     {
-        cout << "thread id:\t" << pthread_self() << endl; 
-        sleep(5);
+        cout << "thread id:\t" << pthread_self() << "\titrer: " << i << endl; 
+        sleep(3);
     }
+
+    pthread_exit(0);
 }
 
-int main()
+
+int main(int argc, char* argv[])
 {
     pthread_t thread1, thread2;
-    pthread_create(&thread1, NULL, threadOneFunct, NULL);
-    pthread_create(&thread2, NULL, threadTwoFunct, NULL);
+    long thread_id_1;
+    long thread_id_2;
+    pthread_create(&thread1, NULL, threadOneFunct, &thread_id_1);
+    pthread_create(&thread2, NULL, threadTwoFunct, &thread_id_2);
     
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "main thread:\t" << pthread_self() << "\titer: " << i << endl;
+        sleep(1);
+    }
+    
+    int code_ex = pthread_join(thread1, NULL);
+    cout << "thread " << thread_id_1 << " ended with code " << code_ex << endl;
+    code_ex = pthread_join(thread2, NULL);
+    cout << "thread " << thread_id_2 << " ended with code " << code_ex << endl;
+
     return 0;
 }
