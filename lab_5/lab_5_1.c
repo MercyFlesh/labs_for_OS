cb4b99a7fa76bf2999a68ebe62114d5cfab828c6
MercyFlesh <con2288@mail.ru>
2020-03-23 13:04:28 +0300

    add lab_5

---
+++ lab_5/lab_5_1.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+struct th_args
+{
+    int th_code;
+    FILE* file;
+    char flag;                                                                                                                                                                                                                                                                                                                                                                
+};
+
+sem_t* sem;  
+
+void* th_func(void* args)
+{
+    errno = 0;
+    char* flag = &((struct th_args* ) args)->flag;
+    FILE* file = ((struct th_args* ) args)->file;
+
+    while((int *)(*flag) != 10)
+    {
+        sleep(1);
+        if(!sem_wait(sem))
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                printf("%c", '1');
+                fflush(stdout);
+                fputc('1', file);
+                fflush(file);
+                sleep(1);
+            }
+            
+            if (sem_post(sem) == -1)
+            {
+                printf("Failed to unlock semaphore whith error: %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+           
+        sleep(1);
+    }
+}
+
+
+int main()
+{
+    int ret;
+    errno = 0;
+    struct th_args params;
+    pthread_t th;
+
+    params.file = fopen("file.txt", "a+");
+    sem = sem_open("semaphore_1", O_CREAT, 0644, 1);
+    void* status;
+    
+    if(pthread_create(&th, NULL, th_func, (void*)(&params)))
+    {
+        printf("Thread create failed with error: %s\n", strerror(errno));
+        return -errno;
+    }
+    
+    
+        
+    params.flag = getchar();
+    
+    if (pthread_join(th, &status) != 0)
+    {
+        printf("Thread join failed with error: %s\n", strerror(errno));
+        return -errno;
+    }
+    close(params.file);
+    sem_close(sem);
+    sem_unlink("semaphore_1");
+    return 0;
+}
